a449bd6fa5b49d30b47b330cf017e8ee2b1040af
Parsa Ghadimi <me@qti3e.com>
2018-08-02 14:42:43 +0430

    Implement the best persian text normalizer :D

--- server/persian.ts
+++ server/persian.ts
@@ -35,6 +35,226 @@ export function normalizeText(text: string): string {
       case "۹":
         tmp += "9";
         break;
+      case "ي":
+      case "ی":
+      case "ﯾ":
+      case "ﯽ":
+      case "ﯿ":
+      case "ﻳ":
+      case "ى":
+      case "ﯾ":
+      case "ﯽ":
+      case "ﯿ":
+      case "ئ":
+      case "ﺋ":
+      case "ﺊ":
+      case "ﺌ":
+      case "ي":
+      case "ﯾ":
+      case "ﯽ":
+      case "ﯿ":
+      case "ﻴ":
+      case "ﻲ":
+        tmp += "ی";
+        break;
+      case "ب":
+      case "ﺑ":
+      case "ﺐ":
+      case "ﺒ":
+        tmp += "ب";
+        break;
+      case "پ":
+      case "ﭘ":
+      case "ﭗ":
+      case "ﭙ":
+        tmp += "پ";
+        break;
+      case "ت":
+      case "ﺗ":
+      case "ﺖ":
+      case "ﺘ":
+        tmp += "ت";
+        break;
+      case "ث":
+      case "ﺛ":
+      case "ﺚ":
+      case "ﺜ":
+        tmp += "ث";
+        break;
+      case "ج":
+      case "ﺟ":
+      case "ﺞ":
+      case "ﺠ":
+        tmp += "ج";
+        break;
+      case "چ":
+      case "ﭼ":
+      case "ﭻ":
+      case "ﭽ":
+        tmp += "چ";
+        break;
+      case "ح":
+      case "ﺣ":
+      case "ﺢ":
+      case "ﺤ":
+        tmp += "ح";
+        break;
+      case "خ":
+      case "ﺧ":
+      case "ﺦ":
+      case "ﺨ":
+        tmp += "خ";
+        break;
+      case "س":
+      case "ﺳ":
+      case "ﺲ":
+      case "ﺴ":
+        tmp += "س";
+        break;
+      case "ش":
+      case "ﺷ":
+      case "ﺶ":
+      case "ﺸ":
+        tmp += "ش";
+        break;
+      case "ص":
+      case "ﺻ":
+      case "ﺺ":
+      case "ﺼ":
+        tmp += "ص";
+        break;
+      case "ض":
+      case "ﺿ":
+      case "ﺾ":
+      case "ﻀ":
+        tmp += "ض";
+        break;
+      case "ط":
+      case "ﻃ":
+      case "ﻂ":
+      case "ﻄ":
+        tmp += "ط";
+        break;
+      case "ظ":
+      case "ﻇ":
+      case "ﻆ":
+      case "ﻈ":
+        tmp += "ظ";
+        break;
+      case "ع":
+      case "ﻋ":
+      case "ﻊ":
+      case "ﻌ":
+        tmp += "ع";
+        break;
+      case "غ":
+      case "ﻏ":
+      case "ﻎ":
+      case "ﻐ":
+        tmp += "غ";
+        break;
+      case "ف":
+      case "ﻓ":
+      case "ﻒ":
+      case "ﻔ":
+        tmp += "ف";
+        break;
+      case "ق":
+      case "ﻗ":
+      case "ﻖ":
+      case "ﻘ":
+        tmp += "ق";
+        break;
+      case "ک":
+      case "ﮐ":
+      case "ﮏ":
+      case "ﮑ":
+      case "ﻛ":
+      case "ﻜ":
+      case "ك":
+      case "ﮐ":
+      case "ﮏ":
+      case "ﮑ":
+      case "ﻚ":
+        tmp += "ک";
+        break;
+      case "گ":
+      case "ﮔ":
+      case "ﮓ":
+      case "ﮕ":
+        tmp += "گ";
+        break;
+      case "ل":
+      case "ﻟ":
+      case "ﻞ":
+      case "ﻠ":
+        tmp += "ل";
+        break;
+      case "م":
+      case "ﻣ":
+      case "ﻢ":
+      case "ﻤ":
+        tmp += "م";
+        break;
+      case "ن":
+      case "ﻧ":
+      case "ﻦ":
+      case "ﻨ":
+        tmp += "ن";
+        break;
+      case "ﻻ":
+      case "ﻼ":
+      case "ﺎ":
+        tmp += "لا";
+        break;
+      case "أ":
+      case "إ":
+      case "ا":
+      case "ﺄ":
+        tmp += "ا";
+        break;
+      case "ﺆ":
+      case "ﻮ":
+      case "و":
+      case "ؤ":
+        tmp += "و";
+        break;
+      case "ﺪ":
+      case "د":
+        tmp += "د";
+        break;
+      case "ذ":
+      case "ﺬ":
+        tmp += "ذ";
+        break;
+      case "ر":
+      case "ﺮ":
+        tmp += "ر";
+        break;
+      case "ز":
+      case "ﺰ":
+        tmp += "ز";
+        break;
+      case "ﮋ":
+      case "ژ":
+        tmp += "ژ";
+        break;
+      case "ة":
+      case "ﮥ":
+      case "ه":
+      case "ه":
+      case "ﻪ":
+      case "ﻬ":
+      case "ﻩ":
+      case "ﻬ":
+      case "ﻫ":
+      case "ﻪ":
+      case "ﻬ":
+        tmp += "ه";
+        break;
+      case "ﷲ":
+        tmp += "الله";
+        break;
       default:
         tmp += text[i];
     }
